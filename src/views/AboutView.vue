b94b2abb89f460a760c287112f9c4735453b2fb4
Caio Finotti Bosco <dandinzin@gmail.com>
2025-09-25 09:03:02 -0300

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card" style="margin-bottom: 100px">
+      <h1 class="text-center">PokeVite</h1>
+      <div class="card-body">
+        <h2>Objetivo:</h2>
+        <p>Projeto criado a partir do curso do professor <a href="https://www.udemy.com/user/yuri-erik-marcon-oliveira/" target="_blank">Yuri Marcon</a> para praticar o desenvolvimento com Vue 3 + Vite.</p>
+        <hr>
+        <h3>Tutorial</h3>
+        <p>
+          Link do GitHub: <a href="https://github.com/yurimarcon/Vite-pokedex" target="_blank" alt="Link do projeto no GitHub" >/Vite-pokedex</a>
+        </p>
+        <p>Me acompanhe no LinkedIn, network é muito importante para área de tecnologia!</p>
+        <a class="btn btn-primary" target="_blank" href="https://link-service.netlify.app/" alt="Minhas mídias sociais">👨‍💻 LinkedIn</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
